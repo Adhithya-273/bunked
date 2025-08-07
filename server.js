1465d149d73da074aba639a365136c73c6ee0629
Adhithya M <adhithyamb273@gmail.com>
2025-08-07 22:05:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 
 // --- Express App Initialization ---
 const app = express();
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 // --- Configuration ---
